3b36f1a2c448caab3f5231c890cd493a6a6a7f0b
Mohammed Abdulatef <github@email.sendithere.co>
2024-11-03 13:16:27 +0300

    auth setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel()
+  integrations: [clerk(), react(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="@clerk/astro/env" />
+/// <reference types="astro/client" />
